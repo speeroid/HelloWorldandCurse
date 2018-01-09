87f08934337a1249fe5cd8420e1a15e6cd997eb9
Speered <Speered@DL-JLP13H2>
2018-01-09 11:28:29 -0800

    Add project files.

---
+++ DAL_Generic/Budget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Generic
+{
+    public class Budget
+    {
+        public int countFacilities()
+        {
+            return 12;
+        }
+        public string curse()
+        {
+            return "Thunder!";
+        }
+    }
+}
